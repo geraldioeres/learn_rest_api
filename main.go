56d0c68bcb0b6af2c2b5a4c1e01fe72a497971ed
geraldiors <geralmers01@gmail.com>
2021-09-23 01:52:02 +0800

    learning orm register user

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
@@ -10,15 +11,25 @@ import (
 )
 
 type User struct {
-	Id       int    `gorm:"primaryKey" json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Address  string `json:"address"`
-	Password string `json:"password"`
+	Id        int            `gorm:"primaryKey" json:"id"`
+	Name      string         `json:"name"`
+	Email     string         `gorm:"unique" json:"email"`
+	Address   string         `json:"address"`
+	Password  string         `json:"password"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
 
 type UserLogin struct {
 	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserRegister struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Address  string `json:"address"`
 	Password string `json:"password"`
 }
 
@@ -50,8 +61,45 @@ func main() {
 	eV1 := e.Group("v1/")
 	eV1.GET("users", GetUserController)
 	eV1.POST("users/login", LoginController)
+	eV1.POST("users/register", RegisterController)
 	eV1.GET("users/:userId", DetailUserController)
 	e.Start(":8000")
+}
+
+func RegisterController(c echo.Context) error {
+	var userRegister UserRegister
+	c.Bind(&userRegister)
+
+	//Validation
+	if userRegister.Name == "" {
+		return c.JSON(http.StatusBadRequest, BaseResponse{
+			Code:    http.StatusBadRequest,
+			Message: "Nama masih kosong",
+			Data:    nil,
+		})
+	}
+	//Etc
+
+	var userDB User
+	userDB.Name = userRegister.Name
+	userDB.Password = userRegister.Password
+	userDB.Address = userRegister.Address
+	userDB.Email = userRegister.Email
+
+	result := DB.Create(&userDB)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, BaseResponse{
+			Code:    http.StatusInternalServerError,
+			Message: "Error ketika input data user ke DB",
+			Data:    nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code:    http.StatusOK,
+		Message: "Berhasil Register",
+		Data:    userDB,
+	})
 }
 
 func LoginController(c echo.Context) error {
@@ -83,16 +131,16 @@ func DetailUserController(c echo.Context) error {
 }
 
 func GetUserController(c echo.Context) error {
-	name := c.QueryParam("name")
-	address := c.QueryParam("address")
+	// name := c.QueryParam("name")
+	// address := c.QueryParam("address")
 	// bisnis
 	user := User{}
 
-	if name == "" {
-		user = User{1, "Alterra", "alterrra@gmail.com", "malang", ""}
-	} else {
-		user = User{1, name, "alterrra@gmail.com", address, ""}
-	}
+	// if name == "" {
+	// 	user = User{1, "Alterra", "alterrra@gmail.com", "malang", ""}
+	// } else {
+	// 	user = User{1, name, "alterrra@gmail.com", address, ""}
+	// }
 
 	return c.JSON(http.StatusOK, BaseResponse{
 		Code:    http.StatusOK,
